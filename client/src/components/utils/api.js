0b5038238737fb5a1b9b22bca65121e5b64f2d94
aviadc <coaviadco@gmail.com>
2022-05-18 17:31:14 +0300

    make it better4

--- client/src/components/utils/api.js
+++ client/src/components/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 let myUrl = 'http://localhost:5000/api/'; //development
 
 if (process.env.NODE_ENV === 'production') {
-  myUrl = 'api'; 
+  myUrl = 'api';  
 }
 const weatherApi =  axios.create({
   baseURL: myUrl,
